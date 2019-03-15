4a75659e6722bd80db4240f665b7094b6fc6655c
pknaul <48594852+pknaul@users.noreply.github.com>
2019-03-15 21:30:46 +0700

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,19 @@
+#define f first
+#define s second
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+    freopen("Inp.txt","r", stdin);
+    freopen("Out.txt","w", stdout);
+   	int n;
+   	cin >> n;
+   	for (int i = 0; i < n; i++)
+   		cout << i+1 << ". Hello" << endl;
+    return 0;
+    
+}
